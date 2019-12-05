368a7cb734346e0b9e43153f0541cbabf48d9242
Christopher Adlam <cadlam@ucdavis.edu>
2019-12-05 13:30:12 -0800

    Some moderately succesfuly GLM efforts; starting to compile specimens

--- scripts/data_load.R
+++ scripts/data_load.R
@@ -2,7 +2,7 @@
 
 # Load packages
 if(!require('pacman'))install.packages('pacman')
-pacman::p_load(tidyverse, emmeans, ggplot2, cowplot, vegan, data.table, kableExtra, dplyr, plyr, nloptr, labdsv, betapart, lattice, rowr, plotly, spdep, bbmle, rsq, lmtest, broom)
+pacman::p_load(tidyverse, emmeans, ggplot2, cowplot, vegan, data.table, kableExtra, dplyr, plyr, nloptr, labdsv, betapart, lattice, rowr, plotly, spdep, bbmle, rsq, lmtest, broom, lmerTest, lme4, emmeans)
 
 ## Site
 site_data2018 <- read.csv("/Users/christopheradlam/Desktop/Davis/R/GitHub Repos/Fire_mosaics/Data/site_data.csv") 
@@ -86,14 +86,18 @@ plant_dat <- left_join(plant_data, plant_names, by = "species") %>%
 plant_mat_pa_w1 <- splist2presabs(plant_dat, sites.col = 1, sp.col = 2)  # same as plant_matrix2
   
 plant_mat_pa_w <- plant_mat_pa_w1[,-1] %>% 
-  select(which(colSums(.[,-1]) > 9)) %>%  # choose threshold for number of detections
+  dplyr::select(which(colSums(.) > 0)) %>%  # choose threshold for number of detections
   cbind(plant_mat_pa_w1[,"site_id"], .)
 
 names(plant_mat_pa_w)[1] <- "site_id" # having to rename first column because the code somehow changed it
 
 plant_dat_pa_w <- plant_mat_pa_w %>% 
   left_join(., site_data, by ="site_id") %>% 
-  mutate(cov_cat = ifelse(tree_cov > 65, 3, ifelse(tree_cov > 35, 2, 1)))  # creating tree cover categories
+  mutate(cov_cat = ifelse(tree_cov > 65, 3, ifelse(tree_cov > 30, 2, 1)))  # creating tree cover categories
+
+#plant_dat_pa_w %>% 
+#  group_by(cov_cat) %>% 
+#  tally()
 
 plant_dat_pa_l <- plant_mat_pa_w %>% 
   pivot_longer(names_to = "species", values_to = "pa", cols = -site_id) 
@@ -108,33 +112,18 @@ plant_mat_cov_w <- spread(data = plant_dat_long, key = species, value = cover, f
 # convert to wide format for following analysis
 #plant_matrix1 <- spread(data = plant_dat, key = species, value = cover, fill = 0)
 
-# optional/incomplete:remove plants with few sightings; not sure this makes much difference
-## Here I'm making a row for the frequency of detection
-
-plant_matrix2 <- plant_mat_pa_w %>% 
-  bind_rows(summarise_all(., funs(if(is.numeric(.)) sum(.) else "Total"))) # adds a 'totals detections' row using p/a counts
-
-# adding the total detections to the cover matrix
-plant_matrix <- plant_mat_cov_w %>%   
-  bind_rows(., plant_matrix2[111,]) #for only 2018 data, change 111 to 49
-  
 # adding dummy species (eg. Webster 2010)
-plant_matrix <- plant_matrix %>% 
-  mutate("DUMB" = "0.5") 
-
-## keep only columns where minimum is reached (if subsetting some sites, change row number, here 49)
-plant_matrix <- plant_matrix[, (plant_matrix[111, ]) > 4]
+#plant_matrix <- plant_matrix %>% 
+#  mutate("DUMB" = "0.5") 
 
 ## adding back in the site id and removing true/false row 
 #(and making sure it's all read in as numeric)
-plant_mat_cov_w <- plant_matrix[-111, ]  #for only 2018 data, use change 111 to 49; this used to be plant_dat_cov, same as plant_mrpp_d;  %>% mutate_if(is.character, as.numeric)
+#plant_mat_cov_w <- plant_matrix[-111, ]  #for only 2018 data, use change 111 to 49; this used to be plant_dat_cov, same as plant_mrpp_d;  %>% mutate_if(is.character, as.numeric)
 
 plant_dat_cov_w <- left_join(plant_mat_cov_w, site_data, by = "site_id") #this used to be plant_dat_cov, same as plant_mrpp_d
 
 plant_dat_cov_l <- plant_mat_cov_w %>% 
-  gather(key = species, value = cover, ABCO:YAMI) # change spp depending on which were removed when subsetting and eliminating based on detection frequency
-
-
+  pivot_longer(names_to = "species", values_to = "pa", cols = -site_id)
 
 
 
@@ -238,7 +227,7 @@ bird_dat_long <- unique(bird_dat_count[ , c(1,2) ]) %>%
 
 # executing function and going from wide to long:
 bird_dat_pa <- splist2presabs(bird_dat_long, sites.col = 1, sp.col = 2) %>% 
-  gather(key = species, value = pa, ACWO:YRWA)
+  pivot_longer(names_to = "species", values_to = "pa", cols = -site_id)
 
 #Add in site data
 site_data$site_id <- as.factor(site_data$site_id)
@@ -246,6 +235,9 @@ bird_dat <- left_join(bird_dat_pa, site_data, by = "site_id")
 
 # convert to wide format for following analysis
 bird_dat_w <- spread(data = bird_dat, key = species, value = pa, fill = 0) #previously bird_matrix1
+
+bird_mat_w <- bird_dat_w %>% 
+  dplyr::select(-c(fire:sev_cov))
 
 # optional:remove birds with few sightings; not sure this makes much difference
 ## add columns true/false depending on obs count reaching minimum value
@@ -282,6 +274,4 @@ all_spp_w_pa <- unique(all_spp[ , c(1:3)]) %>%
 
 all_spp_dat_w <- merge(all_spp_w_pa, site_data, by = "site_id")
 
-all_spp_dat_l <- gather(all_spp_dat_w, key = species, value = pa, ABCO:YRWA)
-
-all_spp_dat_l <- gather(all_spp_dat_w, key = species, value = pa, ABCO:YRWA)
+all_spp_dat_l <- gather(all_spp_dat_w, key = species, value = pa, ACMA:YRWA)
