c1ca49bcb7d9ae2643f6751a637a9d7080d232e2
Christopher Adlam <cadlam@ucdavis.edu>
2020-06-17 19:07:01 -0700

    Lots more changes, better last figure, formatting for pdf output

--- scripts/data_load.R
+++ scripts/data_load.R
@@ -5,7 +5,7 @@ setwd("/Users/christopheradlam/Desktop/Davis/R/GitHub Repos/Fire_mosaics")
 
 # Load packages
 if(!require('pacman'))install.packages('pacman')
-pacman::p_load(tidyverse, emmeans, ggplot2, cowplot, vegan, data.table, kableExtra, dplyr, plyr, nloptr, labdsv, betapart, lattice, rowr, plotly, spdep, bbmle, rsq, lmtest, broom, lmerTest, lme4, emmeans, gridExtra, BiodiversityR, lemon, multcomp, gtable, citr, indicspecies, iNEXT, vegetarian) #gtable is from package lemon
+pacman::p_load(grid, gt, ggpubr, vegan, data.table, kableExtra, dplyr, plyr, nloptr, labdsv, betapart, lattice, rowr, plotly, spdep, bbmle, rsq, lmtest, broom, lmerTest, lme4, emmeans, gridExtra, BiodiversityR, lemon, multcomp, gtable, citr, indicspecies, iNEXT, vegetarian, tidyverse, emmeans, ggplot2, cowplot) #gtable is from package lemon
 #install.packages('tinytex')
 #tinytex::install_tinytex()
 
@@ -27,8 +27,8 @@ site_data_read <- left_join(site_data_read1, site_cwd, by = "site_id") %>% dplyr
 
 
 site_data <- site_data_read %>% 
-  mutate(tsf_cat = ifelse(is.na(tsf), "4", "3")) %>% 
-  mutate(tsf_2cat = ifelse(is.na(tsf), "3", "2"))
+  dplyr::mutate(tsf_cat = ifelse(is.na(tsf), "4", "3")) %>% 
+  dplyr::mutate(tsf_2cat = ifelse(is.na(tsf), "3", "2"))
 
 site_data$tsf_cat[site_data$tsf < 17] <-2
 site_data$tsf_cat[site_data$tsf < 10] <-1
@@ -36,8 +36,8 @@ site_data$tsf_cat[site_data$tsf < 10] <-1
 site_data$tsf_2cat[site_data$tsf < 15] <-1
 
 site_data <- site_data_read %>% 
-  mutate(tsf_cat = ifelse(is.na(tsf), "5", "4")) %>% 
-  mutate(tsf_2cat = ifelse(is.na(tsf), "3", "2"))
+  dplyr::mutate(tsf_cat = ifelse(is.na(tsf), "5", "4")) %>% 
+  dplyr::mutate(tsf_2cat = ifelse(is.na(tsf), "3", "2"))
 
 site_data$tsf_cat[site_data$tsf < 17] <-3
 site_data$tsf_cat[site_data$tsf < 11] <-2
@@ -46,32 +46,33 @@ site_data$tsf_cat[site_data$tsf < 6] <-1
 site_data$tsf_2cat[site_data$tsf < 15] <-1
 
 site_data <- site_data %>% 
-  mutate(sev_tsf = paste(sev, tsf_cat, sep = "-"))  %>% 
-  mutate(sev_tsf2 = paste(sev, tsf_2cat, sep = "-")) %>%
-  mutate(burn = ifelse(sev == "u", 0, 1))
+  dplyr::mutate(sev_tsf = paste(sev, tsf_cat, sep = "-"))  %>% 
+  dplyr::mutate(sev_tsf2 = paste(sev, tsf_2cat, sep = "-")) %>%
+  dplyr::mutate(burn = ifelse(sev == "u", 0, 1))
 
 d2rad <- function(deg) {(deg *pi) / 180}
 
 site_data <- site_data %>% 
-  mutate(folded_aspect180 = 180 - abs(aspect - 180)) %>%
-  mutate(folded_aspect = 180 - abs(aspect - 225)) %>%
-  mutate(lat_rad = d2rad(lat)) %>% 
-  mutate(slope_rad = d2rad(slope)) %>% 
-  mutate(fold_asp_rad = d2rad(folded_aspect)) %>% 
-  mutate(heat_load = 0.339 + 0.808 * cos(lat_rad) * cos(slope_rad) - 0.196 * sin(lat_rad) * sin(slope_rad) - 0.482 * cos(fold_asp_rad) * sin(slope_rad))
+  dplyr::mutate(folded_aspect180 = 180 - abs(aspect - 180)) %>%
+  dplyr::mutate(folded_aspect = 180 - abs(aspect - 225)) %>%
+  dplyr::mutate(lat_rad = d2rad(lat)) %>% 
+  dplyr::mutate(slope_rad = d2rad(slope)) %>% 
+  dplyr::mutate(fold_asp_rad = d2rad(folded_aspect)) %>% 
+  dplyr::mutate(heat_load = 0.339 + 0.808 * cos(lat_rad) * cos(slope_rad) - 0.196 * sin(lat_rad) * sin(slope_rad) - 0.482 * cos(fold_asp_rad) * sin(slope_rad))
 
 site_data$site_id <- as.character(site_data$site_id)
 
-site_data <- site_data[-(111:114),] %>%# remove the LICH sites 
-mutate(cov_cat = ifelse(tree_cov > 65, 3, ifelse(tree_cov > 30, 2, 1))) %>% # creating tree cover categories   
-mutate(sev_cov = ifelse(sev == "u", "u", ifelse(sev == "h", "h", ifelse(sev =="Rx", paste("RX/mult", cov_cat, sep = "-"), ifelse(sev == "multiple", paste("RX/mult", cov_cat, sep = "-"),  ifelse(sev == "multiple/RX", paste("RX/mult", cov_cat, sep = "-"), paste(sev, cov_cat, sep = "-"))))))) %>% 
-  mutate(tsf = ifelse(sev == "u", 100, tsf)) %>% 
-  mutate(sev = ifelse(tree_cov <= 25, "h", sev)) %>%  # this moves 36, 261, 260, 213, 258 from mult to HS
-#  dplyr::filter(tree_cov <= 25 & sev == "multiple") %>% 
-  mutate(sev3 = ifelse(sev == "u" | sev == "l", "lu", sev)) %>% 
-  mutate(tsf_num = ifelse(tsf == "NA", "100", tsf))
+site_data$sev <- as.character(site_data$sev)
 
-#mutate(sev_cov = paste(sev, cov_cat, sep = "-")) %>% 
+site_data <- site_data[-(111:114),] %>%# remove the LICH sites 
+dplyr::mutate(cov_cat = ifelse(tree_cov > 65, 3, ifelse(tree_cov > 30, 2, 1))) %>% # creating tree cover categories   
+dplyr::mutate(sev_cov = ifelse(sev == "u", "u", ifelse(sev == "h", "h", ifelse(sev =="Rx", paste("RX/mult", cov_cat, sep = "-"), ifelse(sev == "multiple", paste("RX/mult", cov_cat, sep = "-"),  ifelse(sev == "multiple/RX", paste("RX/mult", cov_cat, sep = "-"), paste(sev, cov_cat, sep = "-"))))))) %>% 
+  dplyr::mutate(tsf = ifelse(sev == "u", 100, tsf)) %>% 
+  dplyr::mutate(sev = ifelse(tree_cov <= 25, "h", sev)) %>%  # this moves 36, 261, 260, 213, 258 from mult to HS
+#  dplyr::filter(tree_cov <= 25 & sev == "multiple") %>% 
+  dplyr::mutate(sev3 = ifelse(sev == "u" | sev == "l", "lu", sev)) %>% 
+  dplyr::mutate(tsf_num = ifelse(tsf == "NA", "100", tsf))
+#dplyr::mutate(sev_cov = paste(sev, cov_cat, sep = "-")) %>% 
 
 
 ## Plants
@@ -92,30 +93,30 @@ plant_names$form <- as.factor(plant_names$form)
 ## Also for some ecologically similar and taxonomically related plant species, use genus 
 plant_dat <- left_join(plant_data, plant_names, by = "species") %>%
   filter((native_status == "native")) %>% # change if wanting to use subset of species, eg just woody spp, or grass, etc. eg. "& (form == "herb" | form == "grass")""
-  mutate(species = ifelse(species == "CASC", "ASPR", species))  %>% 
-  mutate(species = ifelse(species == "DIHO", "PRHO", species))  %>% 
-  mutate(species = ifelse(species == "DIIM", "DIID", species))  %>% #these three are mistakes
-#  mutate(species = full_name)  %>% 
-  mutate(species = ifelse(genus == "Lupinus", "Lupinus", species)) %>% 
-  mutate(species = ifelse(genus == "Arctostaphylos", "Arctostaphylos", species)) %>% 
-  mutate(species = ifelse(genus == "Penstemon", "Penstemon", species)) %>% 
-  mutate(species = ifelse(genus == "Sidalcea", "Sidalcea", species)) %>% 
-  mutate(species = ifelse(genus == "Iris", "Iris", species)) %>% 
-  mutate(species = ifelse(genus == "Nemophila", "Nemophila", species)) %>% 
-  mutate(species = ifelse(genus == "Agoseris", "Agoseris", species)) %>% 
-  mutate(species = ifelse(genus == "Collinsia", "Collinsia", species)) %>% 
-  mutate(species = ifelse(genus == "Clarkia", "Clarkia", species)) %>% 
-  mutate(species = ifelse(genus == "Pyrola", "Pyrola", species)) %>% 
-  mutate(species = ifelse(genus == "Ribes", "Ribes", species)) %>% 
-  mutate(species = ifelse(genus == "Piperia", "Piperia", species)) %>% 
-  mutate(species = ifelse(genus == "Platanthera", "Piperia", species)) %>% 
-  mutate(species = ifelse(genus == "Madia", "Madia", species)) %>% 
-  mutate(species = ifelse(genus == "Cirsium", "Cirsium", species))  %>% 
-  mutate(species = ifelse(genus == "Silene", "Silene", species))  %>% 
-  mutate(species = ifelse(genus == "Castilleja", "Castilleja", species))  %>% 
-  mutate(species = ifelse(genus == "Cryptantha", "Cryptantha", species))  %>% 
-  mutate(species = ifelse(genus == "Dichelostemma", "Dichelostemma", species))  %>% 
-  mutate(species = ifelse(genus == "Triteleia", "Triteleia", species))  %>% 
+  dplyr::mutate(species = ifelse(species == "CASC", "ASPR", species))  %>% 
+  dplyr::mutate(species = ifelse(species == "DIHO", "PRHO", species))  %>% 
+  dplyr::mutate(species = ifelse(species == "DIIM", "DIID", species))  %>% #these three are mistakes
+#  dplyr::mutate(species = full_name)  %>% 
+  dplyr::mutate(species = ifelse(genus == "Lupinus", "Lupinus", species)) %>% 
+  dplyr::mutate(species = ifelse(genus == "Arctostaphylos", "Arctostaphylos", species)) %>% 
+  dplyr::mutate(species = ifelse(genus == "Penstemon", "Penstemon", species)) %>% 
+  dplyr::mutate(species = ifelse(genus == "Sidalcea", "Sidalcea", species)) %>% 
+  dplyr::mutate(species = ifelse(genus == "Iris", "Iris", species)) %>% 
+  dplyr::mutate(species = ifelse(genus == "Nemophila", "Nemophila", species)) %>% 
+  dplyr::mutate(species = ifelse(genus == "Agoseris", "Agoseris", species)) %>% 
+  dplyr::mutate(species = ifelse(genus == "Collinsia", "Collinsia", species)) %>% 
+  dplyr::mutate(species = ifelse(genus == "Clarkia", "Clarkia", species)) %>% 
+  dplyr::mutate(species = ifelse(genus == "Pyrola", "Pyrola", species)) %>% 
+  dplyr::mutate(species = ifelse(genus == "Ribes", "Ribes", species)) %>% 
+  dplyr::mutate(species = ifelse(genus == "Piperia", "Piperia", species)) %>% 
+  dplyr::mutate(species = ifelse(genus == "Platanthera", "Piperia", species)) %>% 
+  dplyr::mutate(species = ifelse(genus == "Madia", "Madia", species)) %>% 
+  dplyr::mutate(species = ifelse(genus == "Cirsium", "Cirsium", species))  %>% 
+  dplyr::mutate(species = ifelse(genus == "Silene", "Silene", species))  %>% 
+  dplyr::mutate(species = ifelse(genus == "Castilleja", "Castilleja", species))  %>% 
+  dplyr::mutate(species = ifelse(genus == "Cryptantha", "Cryptantha", species))  %>% 
+  dplyr::mutate(species = ifelse(genus == "Dichelostemma", "Dichelostemma", species))  %>% 
+  dplyr::mutate(species = ifelse(genus == "Triteleia", "Triteleia", species))  %>% 
   dplyr::select(site_id, species, cover)
 
 # "mat" is only species data; "dat" also includes site data if in wide format (w), or not if in long format (l)
@@ -130,7 +131,7 @@ names(plant_mat_pa_w)[1] <- "site_id" # having to rename first column because th
 
 plant_dat_pa_w <- plant_mat_pa_w %>% 
   left_join(., site_data, by ="site_id") %>% 
-  mutate(cov_cat = ifelse(tree_cov > 65, 3, ifelse(tree_cov > 30, 2, 1)))  # creating tree cover categories
+  dplyr::mutate(cov_cat = ifelse(tree_cov > 65, 3, ifelse(tree_cov > 30, 2, 1)))  # creating tree cover categories
 
 #plant_dat_pa_w %>% 
 #  group_by(cov_cat) %>% 
@@ -151,11 +152,11 @@ plant_mat_cov_w <- spread(data = plant_dat_long, key = species, value = cover, f
 
 # adding dummy species (eg. Webster 2010)
 #plant_matrix <- plant_matrix %>% 
-#  mutate("DUMB" = "0.5") 
+#  dplyr::mutate("DUMB" = "0.5") 
 
 ## adding back in the site id and removing true/false row 
 #(and making sure it's all read in as numeric)
-#plant_mat_cov_w <- plant_matrix[-111, ]  #for only 2018 data, use change 111 to 49; this used to be plant_dat_cov, same as plant_mrpp_d;  %>% mutate_if(is.character, as.numeric)
+#plant_mat_cov_w <- plant_matrix[-111, ]  #for only 2018 data, use change 111 to 49; this used to be plant_dat_cov, same as plant_mrpp_d;  %>% dplyr::mutate_if(is.character, as.numeric)
 
 plant_dat_cov_w <- left_join(plant_mat_cov_w, site_data, by = "site_id") #this used to be plant_dat_cov, same as plant_mrpp_d
 
@@ -170,34 +171,34 @@ lichen_names <-read.csv("data/lichen_list.csv")
 
 lichen_mat_species_w <- read.csv("data/lichen_data.csv", header = T) %>% 
   filter(site_id != 221) %>% 
-  mutate(site_id = as.factor(site_id)) %>% 
+  dplyr::mutate(site_id = as.factor(site_id)) %>% 
   dplyr::select(site_id, species, abund) %>% 
   spread(key = species, value = abund, fill = 0) %>% 
   full_join(., site_data %>% dplyr::select(site_id), by = "site_id") %>% 
   replace(is.na(.), 0)# %>% 
-#  mutate("DUMB" = 1)  # adding dummy species (eg. Webster 2010) 
+#  dplyr::mutate("DUMB" = 1)  # adding dummy species (eg. Webster 2010) 
 
 #lichen_dat_species_w <- merge(lichen_mat_species_w, site_data, by = "site_id")
 
 #species level analysis
 lichen_dat_species_pa_l <- lichen_mat_species_w %>% 
-  mutate(MECA = ifelse(MECA > 0, MECA, ifelse(MEXX > 0, MEXX, 0))) %>% 
-  mutate(ALSA = ifelse(ALSA > 0, ALSA, ifelse(ALXX > 0, ALXX, 0)))  %>% 
-  mutate(USXX = ifelse(USXX > 0, USXX, ifelse(USTU > 0, USTU, ifelse(USPE > 0, USPE, ifelse(USCA > 0, USCA, ifelse(USSC > 0, USSC, 0)))))) %>% 
-  mutate(XAHA = ifelse(XAHA > 0, XAHA, ifelse(POXX > 0, POXX, 0))) %>% 
+  dplyr::mutate(MECA = ifelse(MECA > 0, MECA, ifelse(MEXX > 0, MEXX, 0))) %>% 
+  dplyr::mutate(ALSA = ifelse(ALSA > 0, ALSA, ifelse(ALXX > 0, ALXX, 0)))  %>% 
+  dplyr::mutate(USXX = ifelse(USXX > 0, USXX, ifelse(USTU > 0, USTU, ifelse(USPE > 0, USPE, ifelse(USCA > 0, USCA, ifelse(USSC > 0, USSC, 0)))))) %>% 
+  dplyr::mutate(XAHA = ifelse(XAHA > 0, XAHA, ifelse(POXX > 0, POXX, 0))) %>% 
   dplyr::select(-c(FUME, FUPU, FUXX, NOPU, WACA, POXX, USTU, USPE, USCA, USSC, ALXX, MEXX)) %>%   # REMOVE THE SQUAMMULOSE SPECIES and generic entries
   gather(key=species, value=abund, AHPA:XAHA) %>% 
   left_join(., lichen_names, by = "species") %>% 
-  mutate(full_name = ifelse(genus == "Usnea", "Usnea sp.", as.character(full_name))) %>% 
-  mutate(full_name = ifelse(species == "NONE", "NONE", as.character(full_name))) %>%   
+  dplyr::mutate(full_name = ifelse(genus == "Usnea", "Usnea sp.", as.character(full_name))) %>% 
+  dplyr::mutate(full_name = ifelse(species == "NONE", "NONE", as.character(full_name))) %>%   
   dplyr::select(site_id,full_name,abund) %>% 
   dplyr::rename(species = full_name) %>% 
-  mutate(pa = ifelse(abund >0, 1, 0)) %>% 
+  dplyr::mutate(pa = ifelse(abund >0, 1, 0)) %>% 
   dplyr::select(site_id,species,pa) 
 
 lichen_mat_species_pa_w <- lichen_dat_species_pa_l %>% 
   spread(key = species, value = pa, fill = 0) %>% 
-  mutate(DUMMY = 1) 
+  dplyr::mutate(DUMMY = 1) 
 
 lichen_dat_species_pa_w <- lichen_mat_species_pa_w  %>% 
   left_join(., site_data, by ="site_id")
@@ -209,17 +210,17 @@ lichen_dat_genus_l <- lichen_mat_species_w %>%
   dplyr::select(-c(FUME, FUPU, FUXX, NOPU, WACA)) %>%   # REMOVE THE SQUAMMULOSE SPECIES
   gather(key=species, value=abund, AHPA:XAHA) %>% 
   left_join(., lichen_names, by = "species") %>% 
-  mutate(species = ifelse(species == "MEXX", "MECA", species)) %>% 
-  mutate(species = ifelse(species == "ALXX", "ALSA", species))  %>% 
-  mutate(species = ifelse(species == "USTU", "USXX", species))  %>% 
-  mutate(species = ifelse(species == "USPE", "USXX", species))  %>% 
-  mutate(species = ifelse(species == "POXX", "XAHA", species))  %>% 
+  dplyr::mutate(species = ifelse(species == "MEXX", "MECA", species)) %>% 
+  dplyr::mutate(species = ifelse(species == "ALXX", "ALSA", species))  %>% 
+  dplyr::mutate(species = ifelse(species == "USTU", "USXX", species))  %>% 
+  dplyr::mutate(species = ifelse(species == "USPE", "USXX", species))  %>% 
+  dplyr::mutate(species = ifelse(species == "POXX", "XAHA", species))  %>% 
   dplyr::select(site_id,full_name,abund)  
 
 
 # remove duplicate rows (same species detected multiple times in a single plot)
 #lichen_dat_genus_pa1 <- lichen_dat_genus_l %>% 
-#  mutate(pa = ifelse(abund == 0, 0, 1)) %>% 
+#  dplyr::mutate(pa = ifelse(abund == 0, 0, 1)) %>% 
 #  dplyr::select(site_id, genus, pa)
 
 #spread not working.
@@ -231,7 +232,7 @@ lichen_dat_genus_l <- lichen_mat_species_w %>%
 #lichen_mat_genus_pa_w <- lichen_dat_genus_pa_l %>% 
 #  spread(key = species, value = pa, fill = 0) %>% 
 #  dplyr::select(c(site_id:Xanthomendoza)) #%>% 
-  #mutate("DUMB" = 1) # need dummy sp?
+  #dplyr::mutate("DUMB" = 1) # need dummy sp?
 
 #lichen_dat_genus_pa_w <- lichen_mat_genus_pa_w %>% 
 #  left_join(., site_data, by ="site_id")
@@ -242,16 +243,16 @@ lichen_dat_genus_l <- lichen_mat_species_w %>%
 
 ## Insects
 insect_dat1 <- read.csv("data/insect_data.csv", header = T) %>% 
-  mutate(order = as.character(order)) %>% 
-  mutate(suborder = as.character(suborder))
+  dplyr::mutate(order = as.character(order)) %>% 
+  dplyr::mutate(suborder = as.character(suborder))
 
 insect_dat2 <- insect_dat1 %>% 
-  mutate(site_id = as.factor(site_id)) %>% 
+  dplyr::mutate(site_id = as.factor(site_id)) %>% 
   #  gsub(" ", "", order) %>% #get rid of spaces in some cells
 #  filter(family != "Curculionidae" & family != "Formicidae" & order != "Arachnida", order != "Apterygota" & order != "Collembola" & order != "Myriapoda") %>% # here, can decide to look at only pan, or only cones
   filter(!family %in% c("Curculionidae", "Formicidae") & !order %in% c("Arachnida", "Apterygota", "Collembola", "Myriapoda", "Ephemeroptera", "Odonata")) %>% #Removing non-arthropods, low catch spp, and bark beetles/ants
-  mutate(trap_id = paste(site_id, trap)) %>% 
-  mutate(taxon = ifelse(suborder %in% "", order, suborder)) # use suborder when available
+  dplyr::mutate(trap_id = paste(site_id, trap)) %>% 
+  dplyr::mutate(taxon = ifelse(suborder %in% "", order, suborder)) # use suborder when available
 
 
 
@@ -261,7 +262,7 @@ insect_mat <- insect_dat3 %>%
   dplyr::select(site_id, taxon, V1) %>% 
   spread(key = taxon, value = V1, fill = 0) %>% 
   dplyr::select(-V1)# %>%
-#  mutate("DUMB" = 1)  # adding dummy species (eg. Webster 2010) 
+#  dplyr::mutate("DUMB" = 1)  # adding dummy species (eg. Webster 2010) 
 
 insect_mat$site_id <- as.character(insect_mat$site_id) # wide format, dummy species, column 2 is garbage
 
@@ -275,7 +276,7 @@ insect_dat_w <- spread(insect_dat_l, key = "taxon", value = "number")
 
 ### Birds
 bird_names <- read.csv("data/bird_spp_list.csv") %>% 
-  mutate(species = Species)
+  dplyr::mutate(species = Species)
 
 bird_dat_suppl <- read.csv("data/bird_data_suppl.csv")
 bird_dat2019 <- read.csv("data/bird_data2019.csv") %>% 
@@ -291,15 +292,15 @@ bird_dat_count <- read.csv("data/bird_data.csv", header = T) %>%
   dplyr::rename(site_id = Point)
   
 # Adding additional species detections (outside count)
-bird_dat_long <- bind_rows(bird_dat_suppl, bird_dat_count) 
+bird_dat_long <- bind_rows(bird_dat_suppl, bird_dat_count)
 
 # remove duplicate rows (same species detected multiple times in a single plot)
 bird_dat_long <- unique(bird_dat_count[ , c(1,2) ]) %>% 
-  mutate(pa = 1) %>% 
+  dplyr::mutate(pa = 1) %>% 
 #  dplyr::rename(site_id = Point) %>% 
 #  dplyr::rename(species = Spp) %>% 
-#  mutate(species = recode(species, BTYW='BTYW/HEWA', RUHU='XXHU', HEWA='BTYW/HEWA')) %>%  #changing all RUHU and ANHU to XXHU; BTYW and HEWA to BTYW/HEWA
-  mutate(species = ifelse(species %in% c("BTYW", "HEWA"), "BTYW/HEWA", ifelse(species %in% c("RUHU", "ANHU"), "XXHU", species))) %>% 
+  dplyr::mutate(species = recode(species, BTYW='BTYW/HEWA', RUHU='XXHU', HEWA='BTYW/HEWA')) %>%  #changing all RUHU and ANHU to XXHU; BTYW and HEWA to BTYW/HEWA
+#  dplyr::dplyr::mutate(species = ifelse(species %in% c("BTYW", "HEWA"), "BTYW/HEWA", ifelse(species %in% c("RUHU", "ANHU"), "XXHU", species))) %>% 
   filter(!is.na(site_id))
 
 # executing function and going from wide to long:
@@ -330,7 +331,7 @@ bird_mat_w <- bird_dat_w %>%
 ## adding back in the site id and removing true/false row (and making sure it's all read in as numeric)
 
 #bird_matrix <- cbind(site_id = bird_matrix1[, 1], bird_matrix[-49, ]) %>%
-#  mutate_if(is.character, as.numeric)
+#  dplyr::mutate_if(is.character, as.numeric)
 
 
 
@@ -338,13 +339,13 @@ bird_mat_w <- bird_dat_w %>%
 
 # All spp
 plant_dat_pa_l1 <- plant_dat_pa_l %>% 
-  mutate("taxon" = "plant")
+  dplyr::mutate("taxon" = "plant")
 
 bird_dat_pa1 <- bird_dat_pa %>% 
-  mutate("taxon" = "bird")
+  dplyr::mutate("taxon" = "bird")
 
 lichen_dat_species_pa_l1 <- lichen_dat_species_pa_l %>% 
-  mutate("taxon" = "lichen")
+  dplyr::mutate("taxon" = "lichen")
 
 all_spp <- rbind(plant_dat_pa_l1, bird_dat_pa1) %>% 
   rbind(., lichen_dat_species_pa_l1) %>% 
@@ -361,3 +362,4 @@ all_spp_w_pa <- unique(all_spp[ , c(1:3)]) %>%
 all_spp_dat_w <- merge(all_spp_w_pa, site_data, by = "site_id")
 
 all_spp_dat_l <- gather(all_spp_dat_w, key = species, value = pa, ABCO:YRWA)
+
--- scripts/pairwise_permanova.R
+++ scripts/pairwise_permanova.R
@@ -4,6 +4,8 @@ pairwise.adonis <- function(x,factors, sim.function = 'vegdist', sim.method = 'b
   library(vegan)
   
   co = combn(unique(as.character(factors)),2)
+  df_mod = c()
+  df_res = c()
   pairs = c()
   F.Model =c()
   R2 = c()
@@ -17,6 +19,8 @@ pairwise.adonis <- function(x,factors, sim.function = 'vegdist', sim.method = 'b
     
     ad = adonis(x1 ~ factors[factors %in% c(co[1,elem],co[2,elem])] );
     pairs = c(pairs,paste(co[1,elem],'vs',co[2,elem]));
+    df_mod = c(df_mod,ad$aov.tab[1,1]);
+    df_res = c(df_res,ad$aov.tab[2,1]);
     F.Model =c(F.Model,ad$aov.tab[1,4]);
     R2 = c(R2,ad$aov.tab[1,5]);
     p.value = c(p.value,ad$aov.tab[1,6])
@@ -28,7 +32,7 @@ pairwise.adonis <- function(x,factors, sim.function = 'vegdist', sim.method = 'b
   sig[p.adjusted <= 0.001] <-'**'
   sig[p.adjusted <= 0.0001] <-'***'
   
-  pairw.res = data.frame(pairs,F.Model,R2,p.value,p.adjusted,sig)
+  pairw.res = data.frame(pairs,df_mod, df_res, F.Model,R2,p.value,p.adjusted,sig)
 #  print("Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1")
   return(pairw.res)
   
